3b0e994ac227df58a36b21fb4ef11343ad010fd6
Connor Shugg <connor@shugg.dev>
2022-10-25 01:07:33 +0000

    added JWT installation to service setup script

--- services/setup/install-dependencies.sh
+++ services/setup/install-dependencies.sh
@@ -7,3 +7,8 @@ sudo apt install python3-pip -y
 echo "Installing flask..."
 python3 -m pip install Flask
 
+echo "Installing jwt..."
+python3 -m pip uninstall JWT
+python3 -m pip uninstall PyJWT
+python3 -m pip install PyJWT
+
