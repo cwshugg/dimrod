0ac0dabbed4d92ad006709af7dd502caaaacfae6
Connor Shugg <connor@shugg.dev>
2023-01-14 20:02:07 -0500

    added email library code

---
+++ services/lib/mail.py
@@ -0,0 +1,52 @@
+# This module implements an interface for sending email. Useful for notifying
+# users of occurrences and events the server has carried out.
+#
+#   Connor Shugg
+
+# Imports
+import os
+import sys
+import threading
+
+# Enable import from the parent directory
+pdir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+if pdir not in sys.path:
+    sys.path.append(pdir)
+
+# Local imports
+from lib.config import Config, ConfigField
+from lib.ifttt import Webhook
+
+
+# =============================== Config Class =============================== #
+# Configuration for the messenger.
+class MessengerConfig(Config):
+    def __init__(self):
+        super().__init__()
+        self.fields = [
+            ConfigField("messenger_webhook_event",    [str],      required=True)
+        ]
+
+
+# ============================= Messenger Class ============================== #
+# Main messenger class.
+class Messenger:
+    # Constructor.
+    def __init__(self, config_path):
+        self.config = MessengerConfig()
+        self.config.parse_file(config_path)
+
+        # set up a IFTTT webhook object
+        self.webhooker = Webhook(config_path)
+    
+    # Takes in an email address, a subject string, and a content string, and
+    # sends an email.
+    def send(self, email: str, subject: str, content: str):
+        email_data = {
+            "to": email,
+            "subject": subject,
+            "content": content
+        }
+        self.webhooker.send(self.config.messenger_webhook_event,
+                            email_data)
+
