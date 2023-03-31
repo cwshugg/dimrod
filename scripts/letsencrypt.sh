7d57ee5cfe9a7f2036ee3ab011dd73de58c7f806
Connor Shugg <connor@shugg.dev>
2023-03-31 09:02:47 -0400

    adjusted certificate setup script

--- scripts/letsencrypt.sh
+++ scripts/letsencrypt.sh
@@ -2,14 +2,26 @@
 # Simple script to help with getting HTTPS working on my Python HTTP services.
 #   https://certbot.eff.org/instructions
 
-# first, install certbot
-echo "Installing certbot..."
-sudo apt install certbot -y
-echo ""
+# get the domain from the command-line
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 yourdomain.com"
+    exit 0
+fi
+domain="$1"
+echo -e "DImROD Domain: ${domain}\n"
+
+# first, install certbot if necessary
+cb="$(which certbot)"
+if [ -z "${cb}" ]; then
+    echo "Installing certbot..."
+    sudo apt install certbot -y
+    echo ""
+fi
 
 # Run certbot
 echo "Generating certificate. Configure it the following way:"
 echo "  1. Choose to launch a standalone server."
 echo "  2. Make sure your network allows for HTTP communication across the internet via port 80."
-sudo certbot certonly
+echo "     (You may have to temporarily port-forward to set up the certificate.)"
+sudo ${cb} certonly -d ${domain}
 
